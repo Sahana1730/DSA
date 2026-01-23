c2fbf671a60976d7fa3c7ebb30faf2176aa598bd
sahana haragapure <sahanaharagapure178@gmail.com>
2026-01-23 23:39:27 +0530

    Add files via upload

---
+++ selection_sort.py
@@ -0,0 +1,26 @@
+my_array = [54, 34, 25, 15, 2, 11, 99, 12]
+
+n = len(my_array)
+for i in range(n-1):
+    min_index = i
+    for j in range(i+1, n):
+        if my_array[j] < my_array[min_index]:
+            min_index = j
+    min_value = my_array.pop(min_index)
+    my_array.insert(i, min_value)
+
+print("Sorted array:", my_array)
+
+
+#improved selection sort
+my_array = [45, 34, 25, 7, 22, 11, 90, 5]
+
+n = len(my_array)
+for i in range(n):
+    min_index = i
+    for j in range(i+1, n):
+        if my_array[j] < my_array[min_index]:
+            min_index = j   
+    my_array[i], my_array[min_index] = my_array[min_index], my_array[i]
+
+print("Sorted array:", my_array)
