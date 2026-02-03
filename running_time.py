69c551966a02df66c2ee168d41cc281917011edc
sahana haragapure <sahanaharagapure178@gmail.com>
2026-02-03 20:08:35 +0530

    Add files via upload

---
+++ running_time.py
@@ -0,0 +1,10 @@
+#run time
+'''each line defines some kind of running time ,to access any data from RAM
+let us say T(n) is run time for n inputs'''
+
+value=10 #t(n)=1
+val =value+5 #t(n)=3 as val take 1 t(n) addition take 1 t(n) and value take 1 t(n)
+#so total t(n)=1+1+1+1=4
+
+print (val) #t(n)=1
+
